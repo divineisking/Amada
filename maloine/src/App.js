8b7c9db8b0c01e61c1395ae9375577c19d339862
divineisking <josefdivine162@gmail.com>
2022-09-16 06:34:28 -0700

    unlimited changes

---
+++ maloine/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Sidebar from './sidebar'
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Sidebar/>
+        
+        <h1> Divine E. Nnodim</h1>
+        <p>
+          Web Developer/Software Engineer.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+
+      
+    </div>
+    
+  );
+}
+
+//export default App;
---
+++ maloine/src/sidebar.js
@@ -0,0 +1,21 @@
+import './sidebar.css'
+
+export default function Sidebar(){
+    return(
+        <div className='sidebar'>
+            <p>
+                Side Bar Menu
+            </p>
+            <ul>
+                <li>
+                    Projects
+                </li>
+                <li>
+                    Socials
+                </li>
+            </ul>
+
+        </div>
+
+    );
+}
