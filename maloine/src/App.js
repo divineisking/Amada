68e328263a6288aced10202bb59fc2fdb02fcb6c
divineisking <josefdivine162@gmail.com>
2022-09-28 07:13:22 -0700

    created form validation

--- maloine/src/App.js
+++ maloine/src/App.js
@@ -3,29 +3,109 @@ import './App.css';
 
 export default function App() {
 
-  const [text, setText] = useState('ABCDe')
-  const [email, setEmail] = useState('')
+  const [formData, setForm] = useState({
+    name:'',
+    username: '',
+    email:'',
+    password:'',
+    confirmPassword:'',
+    gender:'',
+    updates: false,
+    
+  })
 
   function handleChange(e){
-    setText(e.target.value)
+  
+    const {name,value, type, checked} = e.target
+
+    setForm((prevForm)=>{
+                return {...prevForm, 
+                  [name]: type === 'checkbox' ? checked : value }
+    })
+    
   }
-  function handleChange1(e){
-    setEmail(e.target.value)
-  }
+  
   const handleSubmit = (e) => {
     e.preventDefault()
+    setForm({
+      name:'',
+      username: '',
+      email:'',
+      password:'',
+      confirmPassword:'',
+      female:'',
+      male:'',
+      updates: false,     
+    })
+
+    console.log(formData)
   }
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
 
-        <label htmlFor='Name'>Name</label>
-        <input type='text' onChange={handleChange} placeholder='name' value={text} name='Name' />
+      <h3>SIGNUP</h3>
+  
+      <form onSubmit={handleSubmit} className="form">
+
+        <label htmlFor='name'>Name</label>
+        <input type='text' onChange={handleChange} placeholder='Name' name='name' value={formData.name} />
         <br/>
-        <label htmlFor='Email'>Email</label>
-        <input type='text' onChange={handleChange1} placeholder='email' value={email} name='Email'/>
+        <label htmlFor='email'>Email</label>
+        <input type='text' onChange={handleChange} placeholder='Email' name='email' value={formData.email}/>
         <br/>
-        <button type='submit' >Submit</button>
+        <label htmlFor='username'>Username</label>
+        <input type='text' onChange={handleChange} placeholder='Username'  name='username' value={formData.username}/>
+        <br/>
+        <label htmlFor='password'>Password</label>
+        <input type='text' onChange={handleChange} placeholder='Password'  name='password' value={formData.password}/>
+        <br/>
+        <label htmlFor='confirmPassword'>Confirm Password</label>
+        <input type='text' onChange={handleChange} placeholder='Confirm Password'  name='confirmPassword' value={formData.confirmPassword}/>
+        <br/>
+
+        <div className='gender'>
+        <input type='radio' onChange={handleChange} placeholder='FEMALE' name='gender' value='female' checked={formData.gender==='female'} id='female'/>
+        <label htmlFor='female'>Female</label>
+        <br/>
+        <input type='radio' onChange={handleChange} placeholder='MALE' name='gender' value='male' checked={formData.gender ==='male'} id='male'/>
+        <label htmlFor='male'>Male</label>
+        </div>
+        <br/>
+
+        <select>
+          <optgroup>
+            Favourite Colour
+            <option>
+              Black
+            </option>
+            <option>
+              White
+            </option>
+            <option>
+              Pink
+            </option>
+          </optgroup>
+          <optgroup>
+            Favourite Food
+            <option>
+                Ewa
+            </option>
+            <option>
+                Dodo
+            </option>
+            <option>
+                Fufu
+            </option>
+          </optgroup>
+        </select>
+    
+
+        <div className='emailupdates'>
+        <input type='checkbox' onChange={handleChange} name='updates' id='updates' checked={formData.updates}/>
+          <label htmlFor='updates'>I Would like to receive email updates</label>
+        <br/></div>
+        
+        <button type='submit' >SUBMIT</button>
         
       </form>
     </div>
